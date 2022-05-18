7a92fec5041e49c83a3ab3c3f202f5afa4752ae6
RehaniH <rehani44perera@gmail.com>
2022-05-18 17:21:35 +0530

    interfaces in TS

---
+++ interfaces/human.ts
@@ -0,0 +1,29 @@
+//acts as a TS object type, but also can be implemented through a class
+interface Human{
+    firstName:string;
+    age:number;
+
+    greet:() => void;
+}
+
+let me:Human;
+me = {
+    firstName: 'Himasha',
+    age:24,
+    greet(){
+        console.log('Hello !!!')
+    }
+
+}
+
+class Instructer implements Human{
+    firstName: string;
+    age: number;
+    greet(){
+        console.log('Hello !!!');
+    }
+    readTime(){
+        console.log('Its evening time');
+    }
+}
+
